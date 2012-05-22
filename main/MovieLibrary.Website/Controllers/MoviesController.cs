235ad42bf100cf6ef6dcb389b9fd20c81bff2cd9
Amir Barylko <amir@barylko.com>
2012-05-22 12:36:26 -0500

    Added library to movies controller to return contents on Index

--- main/MovieLibrary.Website/Controllers/MoviesController.cs
+++ main/MovieLibrary.Website/Controllers/MoviesController.cs
@@ -5,9 +5,16 @@ namespace MovieLibrary.Website.Controllers
 {
     public class MoviesController : Controller
     {
+        private readonly IMovieLibrary _library;
+
+        public MoviesController(IMovieLibrary library)
+        {
+            _library = library;
+        }
+
         public ActionResult Index()
         {
-            return View();
+            return View(this._library.Contents);
         }
 
     }
