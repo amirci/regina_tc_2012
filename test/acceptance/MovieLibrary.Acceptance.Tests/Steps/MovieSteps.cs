59385209b5f7b3d1147dced225ceee5facf097c8
Amir Barylko <amir@barylko.com>
2012-05-22 11:55:45 -0500

    Implemented browse empty library feature, all passing

--- test/acceptance/MovieLibrary.Acceptance.Tests/Steps/MovieSteps.cs
+++ test/acceptance/MovieLibrary.Acceptance.Tests/Steps/MovieSteps.cs
@@ -1,4 +1,5 @@
-using System;
+using System.Text.RegularExpressions;
+using SharpTestsEx;
 using TechTalk.SpecFlow;
 
 namespace MovieLibrary.Acceptance.Tests.Steps
@@ -6,6 +7,25 @@ namespace MovieLibrary.Acceptance.Tests.Steps
     [Binding]
     public class MovieSteps : BaseSteps
     {
+        [Given(@"I have no movies stored")]
+        public void ClearTheStorage()
+        {
+            // nothing to do empty by default
+        }
 
+        [When(@"I browse the movies")]
+        public void GoToBrowseMoviesPage()
+        {
+            Browser.GoTo("/movies");
+        }
+
+        [Then(@"I should a notification explaining the listing is empty")]
+        public void AssertNotificationExistsOnPage()
+        {
+            Browser.Instance
+                .FindText(new Regex("Sorry no movies available!"))
+                .Should("Can't find the expected message!")
+                .Not.Be.Null();
+        }
     }
 }
